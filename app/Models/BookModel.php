1658d0f5b346775ccd3c743032f164b015e4a096
d-m4z <dmaztren86@gmail.com>
2023-04-24 16:50:46 +0700

    Menambahkan model

---
+++ app/Models/BookModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BookModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'books';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+}
